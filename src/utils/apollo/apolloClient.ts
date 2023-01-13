435ca580ce52f3393f9f8a6354f5b4d72c5a39e4
ahmad-mahmood1 <ahmad.mahmood.a90@gmail.com>
2023-01-13 21:40:25 +0500

    apollo client context function

--- src/utils/apollo/apolloClient.ts
+++ src/utils/apollo/apolloClient.ts
@@ -2,50 +2,39 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { withApollo } from "next-apollo";
 import { PaginatedPosts } from "../../generated/graphql";
 import { setContext } from "@apollo/client/link/context";
+// import withApollo from "./withApollo";
+import { NextPageContext } from "next";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("token");
-  const qid = localStorage.getItem("qid");
-  const cookie = typeof window === "undefined" ? qid : undefined;
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      // authorization: token ? `Bearer ${token}` : "",
-      cookie,
-    },
-  };
-});
-
-const apolloClient = new ApolloClient({
-  link: httpLink,
   credentials: "include",
-  ssrMode: true,
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          posts: {
-            keyArgs: [],
-            merge(
-              existing: PaginatedPosts | undefined,
-              incoming: PaginatedPosts
-            ): PaginatedPosts {
-              return {
-                ...incoming,
-                posts: [...(existing?.posts || []), ...incoming.posts],
-              };
+});
+
+const apolloClient = (ctx: NextPageContext | undefined) =>
+  new ApolloClient({
+    link: httpLink,
+    credentials: "include",
+    ssrMode: true,
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            posts: {
+              keyArgs: [],
+              merge(
+                existing: PaginatedPosts | undefined,
+                incoming: PaginatedPosts
+              ): PaginatedPosts {
+                return {
+                  ...incoming,
+                  posts: [...(existing?.posts || []), ...incoming.posts],
+                };
+              },
             },
           },
         },
       },
-    },
-  }),
-});
+    }),
+  });
 
 export default withApollo(apolloClient);
